5d8669872099354a9b86620e5b4269194f04a3e9
Mohammad Sohail Abbas <65775084+sohail-abbas@users.noreply.github.com>
2020-09-17 17:31:17 +0530

    Commiting by making Parallel Execution False
    Commiting by making Parallel Execution False to check all test cases are running fine

--- frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
+++ frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
@@ -19,7 +19,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
         features={"classpath:features"}
         ,glue= {"StepsDefine","cucumberHooks"}
         ,monochrome=true
-        ,tags = "@Day2"
+        ,tags = "@Firefox"
         ,plugin = {"cucumberHooks.customReportListener"})
 
 @Test
