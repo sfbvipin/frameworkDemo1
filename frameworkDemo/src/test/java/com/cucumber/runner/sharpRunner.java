44484afb93395d9897d039a6e463f389d0294cb7
Mohammad Sohail Abbas <65775084+sohail-abbas@users.noreply.github.com>
2020-09-28 19:15:21 +0530

    sequential Execution
    sequential Execution

--- frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
+++ frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
@@ -20,7 +20,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @Test
 public class sharpRunner extends AbstractTestNGCucumberTests{
     @Override
-    @DataProvider(parallel = true)
+    @DataProvider(parallel = false)
     public Object[][] scenarios() {
         return super.scenarios();
     }
