d337518e579a9f43a8f00558daa4ab9f66dfbe69
rutika07 <rutika.thakkar@sofbang.com>
2021-03-12 18:22:50 +0530

    Updating to runner file to run @Regression

--- frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
+++ frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
         features={"classpath:features"}
         ,glue= {"StepsDefine","cucumberHooks"}
         ,monochrome=true
-        ,tags = "@GetAPI"
+        ,tags = "@Regression"
         ,plugin = {"cucumberHooks.customReportListener"})
 
 @Test
