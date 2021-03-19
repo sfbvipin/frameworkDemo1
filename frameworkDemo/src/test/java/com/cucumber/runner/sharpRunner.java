fc3a3d0a41f52133d7c55e2815497b16f4cf1914
Mohammad Sohail Abbas <65775084+sohail-abbas@users.noreply.github.com>
2021-03-19 15:41:42 +0530

    Commiting Chat Surve
    1. Update runner file to run chat survey test cases

--- frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
+++ frameworkDemo/src/test/java/com/cucumber/runner/sharpRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
         features={"classpath:features"}
         ,glue= {"StepsDefine","cucumberHooks"}
         ,monochrome=true
-        ,tags = "@Regression"
+        ,tags = "@ChatSurvey"
         ,plugin = {"cucumberHooks.customReportListener"})
 
 @Test
